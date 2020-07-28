17cc6b8a29ffa3c22a5b7c7a28cbba564bc3c0ba
hervemangalakitungwa <herve.mangala@gmail.com>
2020-07-29 00:52:16 +0200

    animations

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,48 +1,132 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
 import { Link } from "react-router-dom";
 
-const Hamburger = () => {
+import {
+  staggerText,
+  staggerReveal,
+  fadeInUp,
+  handleHover,
+  handleHoverExit,
+  handleCityReturn,
+  handleCity,
+  staggerRevealClose
+} from "./Animations";
 
-  
+import toronto from "../images/toronto.webp";
+import quebec from "../images/quebec.webp";
+import vancouver from "../images/vancouver.webp";
+import ottawa from "../images/ottawa.webp";
+import montreal from "../images/montreal.webp";
+
+const cities = [
+  { name: "Toronto", image: toronto },
+  { name: "Québec", image: quebec },
+  { name: "Vancouver", image: vancouver },
+  { name: "Ottawa", image: ottawa },
+  { name: "Montreal", image: montreal }
+];
+
+const Hamburger = ({ state }) => {
+  // Create varibles of our dom nodes
+  let menuLayer = useRef(null);
+  let reveal1 = useRef(null);
+  let reveal2 = useRef(null);
+  let cityBackground = useRef(null);
+  let line1 = useRef(null);
+  let line2 = useRef(null);
+  let line3 = useRef(null);
+  let info = useRef(null);
+
+  useEffect(() => {
+    // If the menu is open and we click the menu button to close it.
+    if (state.clicked === false) {
+      // If menu is closed and we want to open it.
+
+      staggerRevealClose(reveal2, reveal1);
+      // Set menu to display none
+      gsap.to(menuLayer, { duration: 1, css: { display: "none" } });
+    } else if (
+      state.clicked === true ||
+      (state.clicked === true && state.initial === null)
+    ) {
+      // Set menu to display block
+      gsap.to(menuLayer, { duration: 0, css: { display: "block" } });
+      //Allow menu to have height of 100%
+      gsap.to([reveal1, reveal2], {
+        duration: 0,
+        opacity: 1,
+        height: "100%"
+      });
+      staggerReveal(reveal1, reveal2);
+      fadeInUp(info);
+      staggerText(line1, line2, line3);
+    }
+  }, [state]);
 
   return (
-    <div className="hamburger-menu">
-      <div className="menu-secondary-background-color"></div>
-      <div className="menu-layer">
-        <div className="menu-city-background"></div>
-        <div className="container">
-          <div className="wrapper">
-            <div className="menu-links">
+    <div ref={el => (menuLayer = el)} className='hamburger-menu'>
+      <div
+        ref={el => (reveal1 = el)}
+        className='menu-secondary-background-color'></div>
+      <div ref={el => (reveal2 = el)} className='menu-layer'>
+        <div
+          ref={el => (cityBackground = el)}
+          className='menu-city-background'></div>
+        <div className='container'>
+          <div className='wrapper'>
+            <div className='menu-links'>
               <nav>
                 <ul>
                   <li>
-                    <Link to="/oppertunities">Oppertunities</Link>
+                    <Link
+                      onMouseEnter={e => handleHover(e)}
+                      onMouseOut={e => handleHoverExit(e)}
+                      ref={el => (line1 = el)}
+                      to='/opportunities'>
+                      Opportunities
+                    </Link>
                   </li>
                   <li>
-                    <Link to="/solutions">Solutions</Link>
+                    <Link
+                      onMouseEnter={e => handleHover(e)}
+                      onMouseOut={e => handleHoverExit(e)}
+                      ref={el => (line2 = el)}
+                      to='/solutions'>
+                      Solutions
+                    </Link>
                   </li>
                   <li>
-                    <Link to="/contact-us">Contact us</Link>
+                    <Link
+                      onMouseEnter={e => handleHover(e)}
+                      onMouseOut={e => handleHoverExit(e)}
+                      ref={el => (line3 = el)}
+                      to='/contact-us'>
+                      Contact us
+                    </Link>
                   </li>
                 </ul>
               </nav>
-              <div className="info">
+              <div ref={el => (info = el)} className='info'>
                 <h3>Our Promise</h3>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Corporis esse cum tempora nihil dicta, vel enim modi quaerat
-                  tempore beatae earum natus molestiae corrupti, deserunt,
-                  magnam laudantium fugit itaque atque dolor eaque id!
-                  Necessitatibus, ut!
+                  The passage experienced a surge in popularity during the 1960s
+                  when Letraset used it on their dry-transfer sheets, and again
+                  during the 90s as desktop publishers bundled the text with
+                  their software.
                 </p>
               </div>
-              <div className="locations">
+              <div className='locations'>
                 Locations:
-                <span>Toronto</span>
-                <span>Montreal</span>
-                <span>Vancouver</span>
-                <span>Québec</span>
-                <span>Ottawa</span>
+                {/* Returning the list of cities */}
+                {cities.map(el => (
+                  <span
+                    key={el.name}
+                    onMouseEnter={() => handleCity(el.image, cityBackground)}
+                    onMouseOut={() => handleCityReturn(cityBackground)}>
+                    {el.name}
+                  </span>
+                ))}
               </div>
             </div>
           </div>
