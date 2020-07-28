fd41bd239fcb276d9bd834cebbf247962dc86142
hervemangalakitungwa <herve.mangala@gmail.com>
2020-07-29 00:32:29 +0200

    update

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Hamburger = () => {
+
+  
+
   return (
     <div className="hamburger-menu">
       <div className="menu-secondary-background-color"></div>
